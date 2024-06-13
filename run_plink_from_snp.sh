7eaad639420cf9cab95d83be0985193a2a997cc1
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-06-13 15:46:01 -0500

    pipeline scripts

--- run_plink_from_snp.sh
+++ run_plink_from_snp.sh
@@ -4,4 +4,4 @@ OUTPUT=$2
 ./call_to_position.sh $SNP_FILE /home/jupyter/snp_pos.txt
 ./call_from_bim.sh /home/jupyter/snp_pos.txt $BFILE /home/jupyter/snp_call.txt
 ./make_snp_file.sh /home/jupyter/snp_call.txt /home/jupyter/snp_mega.txt
-./run_plink.sh $BFILE $OUTPUT $SNP_FILE
+./run_plink.sh /home/jupyter/megachip/megaex_BestOfMultipleCalls_v2 $OUTPUT $SNP_FILE
