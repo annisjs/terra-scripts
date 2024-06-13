29cf413c59232bd4c5d08d7bb5d953d8376a4249
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-06-13 09:52:32 -0500

    -m flag

--- get_exome.sh
+++ get_exome.sh
@@ -3,4 +3,4 @@ EXOME="gs://working-set-redeposit/exomechip"
 echo "Creating directory $HOME/exomechip"
 mkdir $HOME/exomechip
 echo "Downloading Exome data to workspace"
-gsutil -u $GOOGLE_PROJECT cp -r $EXOME $HOME/exomechip
+gsutil -u $GOOGLE_PROJECT -m cp -r $EXOME $HOME/exomechip
