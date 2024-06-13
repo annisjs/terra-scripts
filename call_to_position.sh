bf1d6de9acaf2a31fcb3e41b43e77c7fae5afd7a
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-06-13 15:28:59 -0500

    perform curl checks

--- call_to_position.sh
+++ call_to_position.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 SNP=$1
 xml_response=$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=SNP&id=$SNP")
+
+# Check if curl command was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to retrieve XML data from NCBI."
+    exit 1
+fi
+
+# Check if xml_response is valid XML
+if ! grep -q '<CHRPOS_PREV_ASSM>' <<< "$xml_response"; then
+    echo "Invalid XML response or SNP ID not found."
+    exit 1
+fi
+
 echo "$xml_response" | grep -oP '(?<=<CHRPOS_PREV_ASSM>).*?(?=</CHRPOS_PREV_ASSM>)'
