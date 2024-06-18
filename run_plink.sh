b2005ffc755a24b201d95ced063736c86456101b
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-06-18 08:09:20 -0500

    recodeA --> recode A

--- run_plink.sh
+++ run_plink.sh
@@ -2,5 +2,5 @@
 BFILE="${1:-$HOME/megachip/megaex_BestOfMultipleCalls_v2}"
 OUTPUT="${2:-output}"
 SNP_FILE="${3:-snp.txt}"
-plink --recodeA --bfile $BFILE --out $OUTPUT --extract $SNP_FILE -d !
+plink --recode A --bfile $BFILE --out $OUTPUT --extract $SNP_FILE -d !
 
