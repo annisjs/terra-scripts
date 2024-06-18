b736eccf2579b1824f8a8bdd1aa700a9b2c4a3bf
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-06-18 13:36:05 -0500

    adjusting main script

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@
 ./get_install_cmake.sh &&
 mkdir /home/jupyter/exome_output &&
 mkdir /home/jupyter/mega_output &&
-./run_plink_pipeline.sh /home/jupyter/terra-scripts/snp.txt /home/jupyter/exome_output/exome_output exome n &&
-./run_plink_pipeline.sh /home/jupyter/terra-scripts/snp.txt /home/jupyter/mega_output/mega_output mega n &&
-Rscript setup_phewas.R &&
-nohup Rscript run_phewas.R &
+./run_plink_pipeline.sh /home/jupyter/terra-scripts/snp.txt exome n &&
+./run_plink_pipeline.sh /home/jupyter/terra-scripts/snp.txt mega n &&
+Rscript get_phenotypes.R &&
+Rscript run_phewas.R
