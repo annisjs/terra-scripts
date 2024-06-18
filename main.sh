17c5af37bd023109cc5adefd7847f99367713704
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-06-18 10:55:03 -0500

    make dir

--- main.sh
+++ main.sh
@@ -2,6 +2,8 @@
 ./get_mega.sh &&
 ./get_install_plink.sh &&
 ./get_install_cmake.sh &&
+mkdir /home/jupyter/exome_output
+mkdir /home/jupyter/mega_output
 ./run_plink_pipeline.sh /home/jupyter/terra-scripts/snp.txt /home/jupyter/exome_output/exome_output exome n &&
 ./run_plink_pipeline.sh /home/jupyter/terra-scripts/snp.txt /home/jupyter/mega_output/mega_output mega n &&
 Rscript setup_phewas.R &&
