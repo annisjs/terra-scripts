1f6ded05f5383f3b37aae165b304eaeeda8bdaf3
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-06-18 08:23:24 -0500

    parameter check number change

--- run_plink_pipeline.sh
+++ run_plink_pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
     echo "Usage: $0 <snp_file> <output_folder> <type (mega or exome)> <pos (y/n)>"
     echo "\nsnp_file: a text file containing the SNPs of interest. If using positions instead of calls, set pos = y. Otherwise, set pos = n."
     echo "\noutput_folder: output folder and the name of the raw/log files, eg. home/jupyter/output_folder/output_exome will output output_exome.raw and output_exome.log in output_folder."
@@ -11,12 +11,14 @@ SNP_FILE=$1
 OUTPUT=$2
 TYPE=$3
 POS=$4
+POS_FILE=/home/jupyter/snp_pos.txt
 if [ "$POS" = "n"]; then
     ./call_to_position_file.sh $SNP_FILE /home/jupyter/snp_pos.txt
+    POS_FILE=$SNP_FILE
 fi
 if [ "$TYPE" = "exome" ]; then
     BFILE=/home/jupyter/exomechip/exomechip_001/redeposit_plink_postqc/postQC_redeposit_exomechip_001_v1_202306
-    ./call_from_bim.sh /home/jupyter/snp_pos.txt $BFILE.bim /home/jupyter/snp_call_$TYPE.txt &&
+    ./call_from_bim.sh $POS_FILE $BFILE.bim /home/jupyter/snp_call_$TYPE.txt &&
     ./make_snp_file.sh /home/jupyter/snp_call_$TYPE.txt /home/jupyter/snp_out_$TYPE.txt &&
     for i in /home/jupyter/exomechip/exomechip_*; do
         if [ -d "$i" ]; then
@@ -33,7 +35,7 @@ fi
 
 if [ "$TYPE" = "mega" ]; then
     BFILE=/home/jupyter/megachip/megaex_BestOfMultipleCalls_v2
-    ./call_from_bim.sh /home/jupyter/snp_pos.txt $BFILE.bim /home/jupyter/snp_call_$TYPE.txt &&
+    ./call_from_bim.sh $POS_FILE $BFILE.bim /home/jupyter/snp_call_$TYPE.txt &&
     ./make_snp_file.sh /home/jupyter/snp_call_$TYPE.txt /home/jupyter/snp_out_$TYPE.txt &&
     ./run_plink.sh $BFILE $OUTPUT /home/jupyter/snp_out_$TYPE.txt
 fi
