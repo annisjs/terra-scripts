a771a19c42edc642b44d03bfc8927548b447d6db
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-06-18 10:54:15 -0500

    phewas scripts; main script

---
+++ main.sh
@@ -0,0 +1,8 @@
+./get_exome.sh &&
+./get_mega.sh &&
+./get_install_plink.sh &&
+./get_install_cmake.sh &&
+./run_plink_pipeline.sh /home/jupyter/terra-scripts/snp.txt /home/jupyter/exome_output/exome_output exome n &&
+./run_plink_pipeline.sh /home/jupyter/terra-scripts/snp.txt /home/jupyter/mega_output/mega_output mega n &&
+Rscript setup_phewas.R &&
+nohup Rscript run_phewas.R &
